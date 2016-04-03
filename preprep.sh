21eb839dccf253f8a3cdf1f924613a90732786b0
b1collector <b1collector@gmail.com>
2016-04-02 21:09:48 -0400

    Add preprep script.
    This docker file is designed to be run on a digital ocean box that needs
    initial configuration. This script file is that initial configuration.

---
+++ preprep.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# update the server
+# apt-get update
+# apt-get upgrade
+
+# set a swap file
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
+
+# configure firewall
+# ufw allow ssh
+# ufw allow 25565/tcp
+
+# build the docker file
+docker build -t b1collector/scriptcraft .
+
+# run scriptcraft
+#docker run -it --rm -p 25565:25565 -e OPS=520c7824-cb8c-4d53-bef1-37c15be92d96 b1collector/scriptcraft
